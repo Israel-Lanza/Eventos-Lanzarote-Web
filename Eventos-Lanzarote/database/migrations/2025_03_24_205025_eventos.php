0c140a9a26c7057cac4933490cae1a29c1b4578c
Tania <taniagaral97@gmail.com>
2025-03-24 20:59:32 +0000

    Migraciones eventos, categorias

---
+++ Eventos-Lanzarote/database/migrations/2025_03_24_205025_eventos.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('eventos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion');
+            $table->string('fecha');
+            $table->string('hora'); 
+            $table->string('ubicacion');
+            $table->string('enlace')->nullable();
+            $table->string('imagen')->nullable();
+            $table->decimal('precio',8, 2);
+            $table->char('estado',1)->default('P');
+            $table->string('autor');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('eventos');
+    }
+};
