a621eff543256b3873a0e422ce8d0407ef2816fd
israel <israel16@hotmail.es>
2025-03-24 19:47:10 +0100

    Inicio del proyecto con backend Laravel 12 (sanctum, spatie permission, php unit para pruebas, y frontend react + tailwindcss+ vite + axios + javascript(SWC) +  react-router-dom

---
+++ Eventos-Lanzarote/bootstrap/app.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //Lo que hará es crear un alias (sanctum) para poder trabajar en las rutas con este middleware usando el alias y no toda la ruta  
+        $middleware->alias([
+            'sanctum' => \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
