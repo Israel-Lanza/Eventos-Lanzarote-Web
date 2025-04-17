904928178ce68a202a77e7a81246eb238b003450
Tania <taniagaral97@gmail.com>
2025-04-17 15:15:22 +0100

    Quitar comentarios

--- eventos-backend/app/Http/Controllers/UserController.php
+++ eventos-backend/app/Http/Controllers/UserController.php
@@ -36,29 +36,22 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'nombre'    => 'required|string|max:200',
+            'email'     => 'required|email|unique:users,email',
+            'password'  => 'required|string|min:8',
+            'cif'       => ['required', 'string', 'max:9', 'regex:/^([0-9]{8}[A-Z]|[A-Z][0-9]{7}[A-Z0-9])$/'],
+        ], [
+            'nombre.required'   => 'El nombre es obligatorio.',
+            'email.required'    => 'El correo electrónico es obligatorio.',
+            'email.unique'      => 'Este correo ya está en uso.',
+            'password.required' => 'La contraseña es obligatoria.',
+            'cif.required'      => 'El número de identificación es obligatorio.',
+            'cif.required'      => 'El número de identificación es obligatorio.',
+            'cif.max'           => 'El número de identificación no puede tener más de 9 caracteres.',
+            'cif.regex'         => 'El número de identificación debe tener un formato válido (Ej: A1234567Z).',
+        ]);
 
-        Log::info('Datos del registro:', $request->all());
-
-     /*   $request->merge([
-        'cif' => is_array($request->cif) ? $request->cif[0] : $request->cif,
-    ]); */
-    
-    $request->validate([
-        'nombre'    => 'required|string|max:200',
-        'email'     => 'required|email|unique:users,email',
-        'password'  => 'required|string|min:8',
-        'cif'       => ['required', 'string', 'max:9', 'regex:/^([0-9]{8}[A-Z]|[A-Z][0-9]{7}[A-Z0-9])$/'],
-    ], [
-        'nombre.required'   => 'El nombre es obligatorio.',
-        'email.required'    => 'El correo electrónico es obligatorio.',
-        'email.unique'      => 'Este correo ya está en uso.',
-        'password.required' => 'La contraseña es obligatoria.',
-        'cif.required'      => 'El número de identificación es obligatorio.',
-        'cif.required'      => 'El número de identificación es obligatorio.',
-        'cif.max'           => 'El número de identificación no puede tener más de 9 caracteres.',
-        'cif.regex'         => 'El número de identificación debe tener un formato válido (Ej: A1234567Z).',
-    ]);
-    
 
         $user = new User();
         $user->nombre = $request->nombre;
@@ -88,13 +81,13 @@ class UserController extends Controller
         ], [
             'nombre.string'         => 'El nombre debe ser una cadena de texto.',
             'nombre.max'            => 'El nombre no puede tener más de 200 caracteres.',
-        
+
             'email.email'           => 'El formato del correo no es válido.',
             'email.unique'          => 'Este correo ya está en uso.',
-        
+
             'password.string'       => 'La contraseña debe ser una cadena de texto.',
             'password.min'          => 'La contraseña debe tener al menos 8 caracteres.',
-        
+
             'cif.required'          => 'El número de identificación es obligatorio.',
             'cif.max'               => 'El número de identificación no puede tener más de 9 caracteres.',
             'cif.regex'             => 'El número de identificación debe tener un formato válido (Ej: A1234567Z).',
